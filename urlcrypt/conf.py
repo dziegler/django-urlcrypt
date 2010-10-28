ff87930ef709f54a488b0877f65b80a93ee525ca
dziegler <david.ziegler@gmail.com>
2010-10-28 13:29:13 -0700

    oops, no reverse method on str

--- urlcrypt/conf.py
+++ urlcrypt/conf.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 SECRET_KEY = settings.SECRET_KEY
 # kind of ghetto, is there a better way to do this other than os.urandom?
-OBFUSCATE_KEY = hashlib.sha512(SECRET_KEY).digest() + hashlib.sha512(SECRET_KEY.reverse()).digest()
+OBFUSCATE_KEY = hashlib.sha512(SECRET_KEY).digest() + hashlib.sha512(SECRET_KEY[::-1]).digest()
 
 URLCRYPT_LOGIN_URL = getattr(settings, 'URLCRYPT_LOGIN_URL', settings.LOGIN_URL)
 RUNNING_TESTS = getattr(settings, 'RUNNING_TESTS', False)
