3873ec96191528c597d89bc380a65982291ba377
dziegler <david.ziegler@gmail.com>
2010-10-28 12:46:27 -0700

    make OBFUSCATE_KEY slightly less lame

--- urlcrypt/conf.py
+++ urlcrypt/conf.py
@@ -3,9 +3,8 @@ import hashlib
 from django.conf import settings
 
 SECRET_KEY = settings.SECRET_KEY
-
-# let's make this really long
-OBFUSCATE_KEY = hashlib.sha512(SECRET_KEY[:10]).digest() + hashlib.sha512(SECRET_KEY[10:]).digest() 
+# kind of ghetto, is there a better way to do this other than os.urandom?
+OBFUSCATE_KEY = hashlib.sha512(SECRET_KEY).digest() + hashlib.sha512(SECRET_KEY.reverse()).digest()
 
 URLCRYPT_LOGIN_URL = getattr(settings, 'URLCRYPT_LOGIN_URL', settings.LOGIN_URL)
 RUNNING_TESTS = getattr(settings, 'RUNNING_TESTS', False)
