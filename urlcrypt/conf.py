a6c0c80e27ba84f07068c5b6b45669b8edecde91
dziegler <david.ziegler@gmail.com>
2010-11-03 06:27:47 -0400

    always turn rsa on for testing

--- urlcrypt/conf.py
+++ urlcrypt/conf.py
@@ -6,15 +6,15 @@ SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = getattr(settings, 'SECRET_KEY', 'sekrit')
 RUNNING_TESTS = getattr(settings, 'RUNNING_TESTS', False)
-RUNNING_TESTS = True
-
-# Changing this setting, or SECRET_KEY, invalidates existing tokens, the pycrypto library is required if enabled
-URLCRYPT_USE_RSA_ENCRYPTION = getattr(settings, 'URLCRYPT_USE_RSA_ENCRYPTION', False)
-# if URLCRYPT_USE_RSA_ENCRYPTION is True, the path to an RSA private key file must be set here
 
 if RUNNING_TESTS:
+    URLCRYPT_USE_RSA_ENCRYPTION = True
     URLCRYPT_PRIVATE_KEY_PATH = os.path.join(SCRIPT_DIR, "test", "test_private_key")
 else:
+    # Changing this setting, or SECRET_KEY, invalidates existing tokens, the pycrypto library is required if enabled
+    URLCRYPT_USE_RSA_ENCRYPTION = getattr(settings, 'URLCRYPT_USE_RSA_ENCRYPTION', False)
+    
+    # if URLCRYPT_USE_RSA_ENCRYPTION is True, the path to an RSA private key file must be set here
     URLCRYPT_PRIVATE_KEY_PATH = getattr(settings, 'URLCRYPT_PRIVATE_KEY_PATH', None)
 
 if URLCRYPT_USE_RSA_ENCRYPTION and URLCRYPT_PRIVATE_KEY_PATH is None:
